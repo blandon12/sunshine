5d9632b26511477c4e2ce7a6160fc6968eed35c2
Chunxin Xu <chunxin.xu@worldfirst.com>
2018-08-24 20:27:06 +0100

    init commit

--- sites/all/themes/bootstrap_subtheme/js/homepage_custom.js
+++ sites/all/themes/bootstrap_subtheme/js/homepage_custom.js
@@ -3,4 +3,4 @@ jQuery(document).ready(function() {
   jQuery("#panel-bootstrap-region-recent_wiki .attachment .views-field-field-wiki-image img, #panel-bootstrap-region-recent_blogs .attachment .views-field-field-blog-image img").addClass("img-responsive");
   jQuery("#panel-bootstrap-column-5 .pane-md-wordcloud-4").addClass("hidden-xs hidden-sm");
 
-});
+});.pane-service
--- sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
@@ -77,9 +77,9 @@
   <div class="<?php print $navbar_classes; ?>">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
+<!--      <a class="logo navbar-btn pull-left" href="--><?php //print $front_page; ?><!--" title="--><?php //print t('Home'); ?><!--">-->
+<!--        <img src="--><?php //print $logo; ?><!--" alt="--><?php //print t('Home'); ?><!--" />-->
+<!--      </a>-->
       <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
